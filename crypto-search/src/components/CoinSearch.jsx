e51e7c1c34ee99c43ff37e0b7fa3d33589d342f3
bishh <yashtekavade@gmail.com>
2023-07-01 05:28:49 +0530

    using api displayed all info

--- crypto-search/src/components/CoinSearch.jsx
+++ crypto-search/src/components/CoinSearch.jsx
@@ -1,7 +1,55 @@
 import React from "react";
+import { AiOutlineStar } from "react-icons/ai";
 
-const CoinSearch = () => {
-  return <div>CoinSearch</div>;
+const CoinSearch = ({ coins }) => {
+  console.log(coins);
+  return (
+    <div>
+      <div>
+        <h1>Search Crypto</h1>
+        <form>
+          <input type="text" placeholder="Search a coin" />
+        </form>
+      </div>
+
+      <table>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Coin</th>
+            <th></th>
+            <th>Price</th>
+            <th>24h</th>
+            <th>24h Volume</th>
+            <th>Mkt</th>
+            <th>Last 7 Days</th>
+          </tr>
+        </thead>
+        <tbody>
+          {coins.map((coin) => (
+            <tr>
+              <td>
+                <AiOutlineStar />
+              </td>
+              <td>{coin.market_cap_rank}</td>
+              <td>
+                <div>
+                  <img src={coin.image} alt={coin.id} />
+                  <p>{coin.name}</p>
+                </div>
+              </td>
+              <td>{coin.symbol}</td>
+              <td>{coin.current_price}</td>
+              <td>{coin.price_change_percentage_24h}</td>
+              <td>{coin.total_volume}</td>
+              <td>{coin.market_cap}</td>
+              <td>{coin.sparkline_in_7d.price}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 export default CoinSearch;
--- crypto-search/src/routes/Home.jsx
+++ crypto-search/src/routes/Home.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import CoinSearch from "../components/CoinSearch";
 
-const Home = () => {
+const Home = ({ coins }) => {
+  console.log(coins); // Check the value of the coins prop in the console
   return (
     <div>
-      <CoinSearch />
+      <CoinSearch coins={coins} />
     </div>
   );
 };
