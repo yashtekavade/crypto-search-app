016b771bae6c0386041fe46bbb52d3cdfde9e0a6
bishh <yashtekavade@gmail.com>
2023-07-07 03:30:55 +0530

    trending column and second page

--- crypto-search/src/routes/CoinPage.jsx
+++ crypto-search/src/routes/CoinPage.jsx
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const CoinPage = () => {
-  return <div>CoinPage</div>;
+  const [coin, setCoin] = useState({});
+
+  const url =
+    "https://api.coingecko.com/api/v3/coins/bitcoin?localization=false&sparkline=true";
+
+  useEffect(() => {
+    axios.get(url).then((response) => {
+      setCoin(response.data);
+      console.log(response.data);
+    });
+  }, [url]);
+  return;
+  <div>
+    <div>
+      <img src={coin.image.large} alt="/" />
+    </div>
+  </div>;
 };
 
 export default CoinPage;
--- crypto-search/src/routes/Home.jsx
+++ crypto-search/src/routes/Home.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import CoinSearch from "../components/CoinSearch";
+import Treading from "../components/Trending";
 
 const Home = ({ coins }) => {
   console.log(coins); // Check the value of the coins prop in the console
   return (
     <div>
       <CoinSearch coins={coins} />
+      <Treading />
     </div>
   );
 };
